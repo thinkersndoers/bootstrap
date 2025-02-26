c2d39dfc39c2e5febd8d96b66ea01007964c41c7
Julien Déramond <juderamond@gmail.com>
2025-02-26 21:41:01 +0100

    docs: migration to Astro
    Co-authored-by: HiDeoo <494699+HiDeoo@users.noreply.github.com>

---
+++ site/src/assets/application.js
@@ -0,0 +1,21 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+/*!
+ * JavaScript for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2025 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ * For details, see https://creativecommons.org/licenses/by/3.0/.
+ */
+
+/* eslint-disable import/no-unresolved */
+import sidebarScroll from './partials/sidebar.js'
+// TODO(Astro migration): Do we need to keep this file? If `Code.astro` is enough, remove this line + the file
+// import codeExamples from './partials/code-examples.js'
+import snippets from './partials/snippets.js'
+/* eslint-enable import/no-unresolved */
+
+sidebarScroll()
+// codeExamples()
+snippets()
